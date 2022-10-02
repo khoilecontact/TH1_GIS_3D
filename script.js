a80eef2088a67e7890f043c5d6bea787cf0d9a69
huyvouit <huyvouit@gmail.com>
2022-10-02 22:35:41 +0700

    add hgiang to main

--- script.js
+++ script.js
@@ -82,6 +82,7 @@ require([
   graphicsLayer.add(withProvince(tien_giang));
   graphicsLayer.add(withProvince(an_giang));
   graphicsLayer.add(withProvince(can_tho));
+  graphicsLayer.add(withProvince(hau_giang));
   // cầu
   graphicsLayer.add(withBridge(cau_vam_cong));
   graphicsLayer.add(withBridge(cau_rach_mieu));
